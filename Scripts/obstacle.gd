0f351c2f893d808f1567d40243b290ad7adf9bfd
tafode <marco_meier_6@hotmail.com>
2023-09-04 00:14:11 +0200

    Changed "static obstacle" to "obstacle", changed particle effect, moved scripts

---
+++ Scripts/obstacle.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export
+var damage_value = 1;
+@export
+var movementVector = Vector2(0,0)
+
+func _process(delta):
+	translate(movementVector)
+
+func _on_area_2d_body_entered(_body):
+	if  "Rocket Ship" in _body.name:
+		print("boom boom")
+		# Explosion Animation
+		get_tree().get_first_node_in_group("SFXPlayer")._play_sound(load("res://Assets/Audio/SFX/ping.mp3"), 0)
+		# Damage Ship
+		queue_free()
