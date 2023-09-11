44fb08cc4aae9b2a26258c2db5153518351ac5a9
cyp <cypriane81@gmail.com>
2023-09-11 20:58:54 +0100

    Made fuel functional

--- Scripts/Game/Base/Parts/PartFuel.gd
+++ Scripts/Game/Base/Parts/PartFuel.gd
@@ -6,6 +6,8 @@ var max_fuel: float
 
 var fuel: float
 
+var ui_id: int
+
 func _setup(part: Part):
 	super(part)
 	max_fuel = $FuelComponent.max_fuel
@@ -16,6 +18,8 @@ func _get_fuel_percent() -> float:
 
 func _use_fuel(amount: float) -> float: # takes how much is tried to use and returns amount used
 	fuel -= amount
+	var mod = _get_fuel_percent()
+	$Sprite/Fuel.modulate = Color(mod, mod, mod, 1)
 	if fuel < 0:
 		var amount_used = -fuel
 		fuel = 0
